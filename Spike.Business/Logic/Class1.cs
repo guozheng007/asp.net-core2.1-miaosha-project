42637328de4847118d845dc6f9867878e2bcdc56
zheng.guo <zheng.guo@DESKTOP-OA2FVJM>
2019-07-02 09:57:20 +0800

    first commit

---
+++ Spike.Business/Logic/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spike.Business.Logic
+{
+    class Class1
+    {
+    }
+}
---
+++ Spike.Business/Verify/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spike.Business.Verify
+{
+    class Class1
+    {
+    }
+}

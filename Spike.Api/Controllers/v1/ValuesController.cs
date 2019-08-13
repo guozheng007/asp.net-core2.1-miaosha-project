70b4ec111e6921fd0ce357d3a5055bb3c016d2fe
zheng.guo <zheng.guo@DESKTOP-OA2FVJM>
2019-08-13 10:49:03 +0800

    1212

--- Spike.Api/Controllers/v1/ValuesController.cs
+++ Spike.Api/Controllers/v1/ValuesController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Spike.Business;
 using Spike.Utility;
+using System.Collections.Generic;
 
 namespace Spike.Api.Controllers.v1
 {
